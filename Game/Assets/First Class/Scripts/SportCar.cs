b19ea9e024918af073c4aa80d41c112d8a1f6bc9
chajungto <50168549+chajungto@users.noreply.github.com>
2024-01-13 17:52:05 +0900

    (2024.01.13) 유니티 프로젝트 업로드

---
+++ Game/Assets/First Class/Scripts/SportCar.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SportCar : MonoBehaviour
+{
+    private float speed = 5.0f;
+
+    protected string carName = "sonata";
+
+    public Vector3 direction;
+}
---
+++ Game/Assets/Second Class/Scripts/AnimatorManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    public Animator animator;
+
+    public void Run()
+    {
+        animator.SetTrigger("Run");
+    }
+
+}
